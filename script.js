711f313659c781f26d2bb268547bf339986f0df2
shubham kanase <shubhamkanase92@gmail.com>
2023-04-27 23:43:31 +0530

    add some functions

---
+++ script.js
@@ -0,0 +1,9 @@
+const eight = document.querySelector("eight");
+const sixteen = document.querySelector("sixteen");
+const thirtyTwo = document.querySelector("thirtyTwo");
+const gridContainer =document.querySelector("grid-container");
+
+
+function creatGrid8(){
+    sixteen.addEventListener
+}
