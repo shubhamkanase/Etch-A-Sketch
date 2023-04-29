e23914fcf8ff6a2e7cbf385273ae362a637810bb
shubham kanase <shubhamkanase92@gmail.com>
2023-04-29 12:08:09 +0530

    creating a grid

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const eight = document.querySelector("eight");
-const sixteen = document.querySelector("sixteen");
-const thirtyTwo = document.querySelector("thirtyTwo");
-const gridContainer =document.querySelector("grid-container");
 
+const container = document.querySelector('#grid-container');
 
-function creatGrid8(){
-    sixteen.addEventListener
+function createGrid(size) {
+  // clear previous grid
+  container.innerHTML = '';
+
+  // set number of rows and columns
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  // create cells
+  for (let i = 0; i < size * size; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    container.appendChild(cell);
+  }
+}
+
+function createGrid8() {
+  createGrid(8);
+}
+
+function createGrid16() {
+  createGrid(16);
+}
+
+function createGrid32() {
+  createGrid(32);
 }
